cc723af45d79a528da403d93b089b82ec8266b53
Mehyar Farzat <mehyar.farzat@gmail.com>
2023-09-12 23:22:28 +0200

    install django-admin-charts and django-light - add admin_tools_stats and django_nvd3 apps to installed apps

--- project/settings.py
+++ project/settings.py
@@ -31,6 +31,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+
+    'admin_tools_stats',
+    'django_nvd3',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
